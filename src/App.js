c02b7244b230f418fc4fbf8b8bd58b75d23733e0
sharadbhat-98 <sharathbhat563@gmail.com>
2021-02-15 02:30:49 +0530

    TODO redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,158 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState,useEffect } from 'react';
+import Button from '@material-ui/core/Button'
+import Textfield from '@material-ui/core/TextField'
+import Grid from '@material-ui/core/Grid'
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent'
+import TextField from '@material-ui/core/TextField';
+import DialogActions from '@material-ui/core/DialogActions';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography'
+import store from './redux/store';
+import * as actions from './redux/actiontypes'
+import { Provider,useSelector } from 'react-redux';
+
 
 function App() {
+ // const unsubscribe=store.subscribe(()=>{
+  //  console.log('store changed',store.getState());
+ // })
+
+  const [item,setItem]=useState([])
+  const [todo,SetTodo]=useState()
+  const [update,setUpdate]=useState()
+  const [open, setOpen] = React.useState(false);
+  const [updateid, setUpdateId]=useState()
+
+  const theme = createMuiTheme({
+    typography: {
+      fontFamily: [
+        'Roboto',
+        'sans-serif'
+  
+      ].join(','),
+  
+    }})
+
+
+  const handleClickOpen = (e) => {
+    console.log(e.id)
+    setUpdateId(e.id)
+    setOpen(true);
+    
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    
+  };
+
+
+  const handleChange=(e)=>{
+    const data=e.target.value
+    SetTodo(data)
+  
+  }
+
+  const handleChangeUpdate=(e)=>{
+    const data=e.target.value
+    setUpdate(data)
+ 
+  }
+  
+  const AddTodo=()=>{
+    store.dispatch({
+      type:actions.ADD_TODO,
+      payload:{
+        agenda:todo
+      }
+    })
+  }
+  const DeleteTodo=(e)=>{
+    console.log(e.id)
+    store.dispatch({
+      type:actions.REMOVE_TODO,
+      payload:{
+        id:e.id
+      }
+    })
+  }
+  const UpdateTodo=(e)=>{
+
+    console.log(updateid)
+    store.dispatch({
+      type:actions.UPDATE_TODO,
+      payload:{
+        id:updateid,
+        agenda:update
+      }
+    })
+    handleClose()
+  }
+
+
+  
+
+ const data=useSelector((state)=> state)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    <ThemeProvider theme={theme}>
+    <div style={{textAlign:"center", marginTop:'5%'}}>
+
+      <Textfield 
+      label='TODO'
+      placeholder="enter a agenda"
+      variant='outlined'
+      onChange={handleChange}
+      />&nbsp; &nbsp;
+      <Button variant="contained" color='primary'style={{padding:'15px',borderRadius:'15px'}} onClick={AddTodo} >ADD</Button>
+
+      <Grid container style={{marginTop:'5%'}} >
+      {data.map((e)=>{
+     
+        return ( <React.Fragment key={e.id}>
+         <Grid item xs={6} style={{border:'0.1px solid 	#C0C0C0'}} >
+           <Typography variant='h6' style={{textAlign:'right',paddingTop:'8px'}}>{e.agenda}</Typography>
+         </Grid>
+         <Grid item xs={6} style={{border:'0.1px solid 	#C0C0C0',textAlign:'left'}} >
+         &nbsp; &nbsp; <Button variant="contained" color='primary' size='small' style={{marginTop:'10px'}} onClick={()=>handleClickOpen(e)}>UPDATE</Button>
+         <Button variant="contained" color='secondary' style={{position:'absolute',marginLeft:'10px',marginTop:'10px'}} size='small' onClick={()=>DeleteTodo(e)}>DELETE</Button>
+
+         <Dialog open={open} onClose={handleClose} >
+           <DialogTitle>UPDATE TODO</DialogTitle>
+           <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            label=" TODO"
+         
+            fullWidth
+            onChange={handleChangeUpdate}
+            required 
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button  color="primary" onClick={()=>UpdateTodo(e)}>
+            Update
+          </Button>
+        </DialogActions>
+
+         </Dialog>
+           
+         </Grid>
+         </React.Fragment>)
+    
+      })}
+       </Grid>
     </div>
+    </ThemeProvider>
+
   );
 }
 
---
+++ src/redux/reducer.js
@@ -0,0 +1,27 @@
+import * as actions from './actiontypes'
+
+let id=0;
+
+const reducer=(state=[],action)=>{
+    switch(action.type){
+        case actions.ADD_TODO:
+            return [
+                ...state,
+                {
+                    id:++id,
+                    agenda:action.payload.agenda,
+                }
+            ];
+        case actions.REMOVE_TODO:
+            return state.filter((e)=>e.id !== action.payload.id)
+
+        case actions.UPDATE_TODO:
+            return state.map(bug => bug.id !== action.payload.id ? bug : {...bug, agenda:action.payload.agenda})
+        default:
+            return state
+
+    }
+
+}
+
+export default reducer
